5ed8556bc28507e42f57f3424a8cdff6fb36c2db
teepeekee <teerti13@gmail.com>
2024-08-12 12:02:28 +0800

    Add project files.

---
+++ MK2/Form1.Designer.vb
@@ -0,0 +1,107 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        lblLogmasuk = New Label()
+        txtId = New TextBox()
+        lblIdpengguna = New Label()
+        lblKatalaluan = New Label()
+        txtKatalaluan = New TextBox()
+        btnLogin = New Button()
+        SuspendLayout()
+        ' 
+        ' lblLogmasuk
+        ' 
+        lblLogmasuk.AutoSize = True
+        lblLogmasuk.Location = New Point(142, 37)
+        lblLogmasuk.Name = "lblLogmasuk"
+        lblLogmasuk.Size = New Size(90, 20)
+        lblLogmasuk.TabIndex = 0
+        lblLogmasuk.Text = "LOG MASUK"
+        ' 
+        ' txtId
+        ' 
+        txtId.Location = New Point(153, 72)
+        txtId.Name = "txtId"
+        txtId.Size = New Size(189, 27)
+        txtId.TabIndex = 1
+        ' 
+        ' lblIdpengguna
+        ' 
+        lblIdpengguna.AutoSize = True
+        lblIdpengguna.Location = New Point(41, 79)
+        lblIdpengguna.Name = "lblIdpengguna"
+        lblIdpengguna.Size = New Size(106, 20)
+        lblIdpengguna.TabIndex = 2
+        lblIdpengguna.Text = "ID PENGGUNA"
+        ' 
+        ' lblKatalaluan
+        ' 
+        lblKatalaluan.AutoSize = True
+        lblKatalaluan.Location = New Point(41, 143)
+        lblKatalaluan.Name = "lblKatalaluan"
+        lblKatalaluan.Size = New Size(103, 20)
+        lblKatalaluan.TabIndex = 3
+        lblKatalaluan.Text = "KATA LALUAN"
+        ' 
+        ' txtKatalaluan
+        ' 
+        txtKatalaluan.Location = New Point(153, 136)
+        txtKatalaluan.Name = "txtKatalaluan"
+        txtKatalaluan.Size = New Size(189, 27)
+        txtKatalaluan.TabIndex = 4
+        ' 
+        ' btnLogin
+        ' 
+        btnLogin.Location = New Point(150, 194)
+        btnLogin.Name = "btnLogin"
+        btnLogin.Size = New Size(94, 29)
+        btnLogin.TabIndex = 5
+        btnLogin.Text = "LOGIN"
+        btnLogin.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(394, 256)
+        Controls.Add(btnLogin)
+        Controls.Add(txtKatalaluan)
+        Controls.Add(lblKatalaluan)
+        Controls.Add(lblIdpengguna)
+        Controls.Add(txtId)
+        Controls.Add(lblLogmasuk)
+        Name = "Form1"
+        Text = "Login Page"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents lblLogmasuk As Label
+    Friend WithEvents txtId As TextBox
+    Friend WithEvents lblIdpengguna As Label
+    Friend WithEvents lblKatalaluan As Label
+    Friend WithEvents txtKatalaluan As TextBox
+    Friend WithEvents btnLogin As Button
+
+End Class
